74b2ea7c66a9d6368e0303ece9041d95ca5e7078
Suhas Sriramaneni <suhaschoudary.s@gmail.com>
2026-07-31 07:35:02 +0000

    Connect frontend to Render backend

--- api/healthz.ts
+++
@@ -1,5 +0,0 @@
-export default function handler(_req: any, res: any) {
-  res.setHeader("Content-Type", "application/json");
-  res.statusCode = 200;
-  res.end(JSON.stringify({ status: "ok" }));
-}
